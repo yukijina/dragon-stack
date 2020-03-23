0650d1bcba0de8ad599ab355aeaeff84b924885a
yukijina <yukijina@gmail.com>
2020-03-22 20:24:57 -0700

    sec.97 create account table and update configuration

--- bin/configure_db.sh
+++ bin/configure_db.sh
@@ -4,6 +4,7 @@ echo "Configureing dragondb"
 dropdb -U node_user dragondb
 createdb -U node_user dragondb
 
+psql -U node_user dragondb < ./bin/sql/account.sql
 psql -U node_user dragondb < ./bin/sql/generation.sql
 psql -U node_user dragondb < ./bin/sql/dragon.sql
 psql -U node_user dragondb < ./bin/sql/trait.sql
