7d66d84e3876034b62f7f9c266273423a718be35
yukijina <yukijina@gmail.com>
2020-03-25 14:42:00 -0700

    sec.107 add functions to session class. Make sure Hash and session is equal

--- app/account/session.js
+++ app/account/session.js
@@ -1,5 +1,7 @@
-const uuid = rewuire('uuid/v4');
+const uuid = require('uuid/v4');
 const { hash } = require('./helper');
+
+const SEPARATOR = '|';
 
 class Session {
   constructor({ username }) {
@@ -10,13 +12,28 @@ class Session {
 
   toString() {
     const { username, id } = this;
-
     return Session.sessionString({ username, id})
+  }
+
+  static parse(sessionString) {
+    const sessionData = sessionString.split(SEPARATOR);
+
+    return {
+      username: sessionData[0],
+      id: sessionData[1],
+      sessionHash: sessionData[2]
+    }
+  }
+
+  static verify(sessionString) {
+    const { username, id, sessionHash } = Session.parse(sessionString);
+    const accountData = Session.accountData({ username, id });
+    return hash(accountData) === sessionHash;
   }
 
   static accountData({ username, id }) {
     // we use pipes (|) between username and id - it just combined username and id ex.john|idgr8gergug
-    return `${username}|${id}`;
+    return `${username}${SEPARATOR}${id}`;
   }
 
   static sessionString({ username, id }) {
@@ -25,5 +42,12 @@ class Session {
     return `${accountData}|${hash(accountData)}`;
   }
 }
+
+//// For debugging purpose: in the terminal, ./node_modules/nodemon/bin/nodemon.js app/account/session.js
+// const foo = new Session({ username: 'foo'});
+
+// const fooString = foo.toString()
+// console.log('Here is Session.parse(fooString)!!: ', Session.parse(fooString))
+// console.log('Here is Session.verify(fooString)!!: ', Session.verify(fooString))
 
 module.exports = Session;
