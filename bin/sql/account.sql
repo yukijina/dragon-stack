aa603acb7d54a6d6f9faf2c2e95b5b25254c7ed7
yukijina <yukijina@gmail.com>
2020-03-26 16:07:54 -0700

    sec.109 add sessionId

--- bin/sql/account.sql
+++ bin/sql/account.sql
@@ -1,7 +1,8 @@
 CREATE TABLE account(
   id             SERIAL PRIMARY KEY,
   "usernameHash" CHARACTER(64),
-  "passwordHash" CHARACTER(64)
+  "passwordHash" CHARACTER(64),
+  "sessionId"    CHARACTER(36)
 );
 
 -- secure password safely. converd plain text password to hased password - use camelcase so need double quote " "
