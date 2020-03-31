beb09901a444374a6111cd2e70d4aab44d96d23d
yukijina <yukijina@gmail.com>
2020-03-30 20:01:07 -0700

    sec.112 create router login

--- app/api/account.js
+++ app/api/account.js
@@ -31,4 +31,22 @@ router.post('/signup', (req, res, next) => {
   .catch(error => next(error))
 })
 
+router.post('/login', (req, res,next) => {
+  const {username, password } = req.body;
+
+  AccountTable.getAccount({ usernameHash: hash(username) })
+  .then(({ account }) => {
+    if (account && account.passwordHash === hash(password)) {
+      return setSession({ username, res })
+    } else {
+      const error = new Error('Incorrect username/password')
+      error.statusCode = 409;
+
+      throw error;
+    }
+  })
+  .then(({ message }) => res.json({ message }))
+  .catch(error => next(error));
+})
+
 module.exports = router;
