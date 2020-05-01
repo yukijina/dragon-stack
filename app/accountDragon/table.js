98d363bb3735c6b6ffd37b9cb0d096781afa3e09
yukijina <yukijina@gmail.com>
2020-04-30 21:39:14 -0700

    sec.136 create accountDragon table to insert the data to the table

---
+++ app/accountDragon/table.js
@@ -0,0 +1,24 @@
+const pool = require('../../databasePool');
+
+class AccountDragonTable {
+  static storeAccountDragon({ accountId, dragonId }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'INSERT INTO accountDragon("accountId", "dragonId") VALUES($1, $2)',
+        [accountId, dragonId],
+        (error, response) => {
+          if (error) return reject(error) 
+          resolve();
+        }
+      )
+    })
+  }
+}
+
+//test debugging
+AccountDragonTable.storeAccountDragon({
+  accountId: 1, dragonId: 3
+}).then(() => console.log('stored account dragon'))
+.catch(error => console.error('error', error));
+
+module.exports = AccountDragonTable;
