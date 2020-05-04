052a4364d8220ccc019c2fcc3b3b2d8af9d2973d
yukijina <yukijina@gmail.com>
2020-05-03 21:26:01 -0700

    sec.138 refactor - organize the functions

--- app/api/dragon.js
+++ app/api/dragon.js
@@ -1,17 +1,28 @@
 const { Router } = require('express');
 const DragonTable = require('../dragon/table');
+const AccountDragonTable = require('../accountDragon/table');
+const { authenticatedAccount } = require('./helper');
 
 const router = new Router();
 
 router.get('/new', (req, res, next) => {
-  const dragon = res.app.locals.engine.generation.newDragon();
-  DragonTable.storeDragon(dragon)
-  .then(({ dragonId }) => {
-    console.log('dragonId', dragonId);
-    dragon.dragonId = dragonId;
-    res.json({ dragon });
+  let accountId, dragon;
+
+  authenticatedAccount({ sessionString: req.cookies.sessionString })
+  .then(({ account }) => {
+    accountId = account.id;
+
+    dragon = req.app.locals.engine.generation.newDragon();
+
+    return DragonTable.storeDragon(dragon)
+
   })
-  .catch(err => next(err));   // this err trigger index.js app.use handler (line 17)
+  .then(({ dragonId }) => {
+    dragon.dragonId = dragonId;
+    return AccountDragonTable.storeAccountDragon({ accountId, dragonId });
+   })
+   .then(() => res.json({ dragon }))
+   .catch(err => next(err));   // this err trigger index.js app.use handler (line 17) 
 });
 
 module.exports = router;
