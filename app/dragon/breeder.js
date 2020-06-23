fd01f80755df78c827b3eafc70e356daebe2d149
yukijina <yukijina@gmail.com>
2020-06-22 20:43:24 -0700

    sec.173 pick trait, create baby dragon, install base-64

--- app/dragon/breeder.js
+++ app/dragon/breeder.js
@@ -1,4 +1,6 @@
+const base64 = require('base-64');
 const Dragon = require('./index');
+
 
 class Breeder {
   static breedDragon({ matron, patron }){
@@ -12,7 +14,7 @@ class Breeder {
 
         const patronTrait = patronTraits.find(
           trait => trait.traitType === traitType
-        )
+        ).traitValue
 
         babyTraits.push({
           traitType, 
@@ -23,6 +25,40 @@ class Breeder {
       return new Dragon({ nickname: 'Unnamed baby', traits: babyTraits })
   }
 
+  // two incoming traits: matronTrait and patronTrait
+  // The matronTrait and patronTrait string values are encoded
+  // Both traits have their characters summed
+  // Get a range by adding both character sums.
+  //generate a random number in that range
+  // If the number is less than the matron's character sum, pick matron.
+  // Else, pick patron
+  static pickTrait({ matronTrait, patronTrait }) {
+    if (matronTrait === patronTrait) return matronTrait;
+
+    const matronTraitCharSum = Breeder.charSum(base64.encode(matronTrait))
+    const patronTraitCharSum = Breeder.charSum(base64.encode(patronTrait))
+
+    const randNum = Math.floor(Math.random() * (matronTraitCharSum + patronTraitCharSum))
+    
+    return randNum < matronTraitCharSum ? matronTrait : patronTrait;
+  }
+
+  static charSum(string) {
+    return string.split('').reduce(
+      (sum, character) => sum += character.charCodeAt(), 0) //0 - initial state
+  }
+
 }
+
+//debugging code
+const fooby = new Dragon();
+const gooby = new Dragon();
+
+console.log('fooby', fooby)
+console.log('gooby', gooby)
+
+const fg = Breeder.breedDragon({ matron: fooby, patron: gooby})
+
+console.log('fg', fg)
 
 module.exports = Breeder;
